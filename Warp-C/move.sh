af7dc1835e1c395d582058932581c17217e2ceec
Roman54228 <71009563+Roman54228@users.noreply.github.com>
2022-10-31 23:55:04 +0300

    Update move.sh

--- Warp-C/move.sh
+++ Warp-C/move.sh
@@ -2,3 +2,7 @@ mkdir all_train
 cd train_crops
 cp -r bottle/* canister/* cans/* cardboard/* detergent/* ../all_train
 cd ..
+
+mkdir all_test
+cd test_crops
+cp -r bottle/* canister/* cans/* cardboard/* detergent/* ../all_test
